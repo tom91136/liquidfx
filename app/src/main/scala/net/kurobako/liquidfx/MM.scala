ed30f0268f658f009701b12a355be1bb255fad0f
Tom Lin <tom91136@gmail.com>
2019-03-01 21:18:25 +0000

    Proper test of MMed data

--- app/src/main/scala/net/kurobako/liquidfx/MM.scala
+++ app/src/main/scala/net/kurobako/liquidfx/MM.scala
@@ -5,16 +5,39 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, OpenOption, Paths, StandardOpenOption}
 import java.time.Instant
 
-import net.kurobako.liquidfx.SphSolver.Vec3
+import net.kurobako.liquidfx.Metaball.Triangle
+import net.kurobako.liquidfx.SphSolver.{Particle, Vec3}
+import scalafx.Includes.handle
+import scalafx.Includes._
+import scalafx.animation.Animation
+import scalafx.application.{JFXApp, Platform}
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.{AmbientLight, Group, Scene}
+import scalafx.scene.control.{Button, Label, Slider, ToggleButton}
+import scalafx.scene.layout.{HBox, Priority, StackPane, VBox}
+import scalafx.scene.paint.{Color, PhongMaterial}
+import scalafx.scene.shape._
+import scalafx.stage.FileChooser
 
-object MM {
-	def main(args: Array[String]): Unit = {
+object MM extends JFXApp {
 
 
-		import java.io.RandomAccessFile
-		import java.nio.MappedByteBuffer
+	val ambient = new AmbientLight(Color.LightYellow)
+
+
+	def updateMesh(mesh: TriangleMesh, xs: Seq[Triangle]) = {
+		mesh.points = xs.flatMap(_.points).map(_.toFloat).toArray
+		mesh.texCoords = Array(0, 0)
+		mesh.faces = Array.tabulate(xs.length * 3)(_ :: 0 :: Nil).flatten
+	}
+
+	val pcount            = 3000
+	val xs: Array[Sphere] = Array.tabulate(pcount) { i => new Sphere(5) }
+
+	new Thread({ () =>
+
 		import java.nio.channels.FileChannel
-		val file = Paths.get("/home/tom/Desktop/ipc")
+		val file = Paths.get("/home/tom/libfluid/cmake-build-release/ipc.mmf")
 
 		def time[R](name: String)(block: => R): R = {
 			val t0 = System.nanoTime()
@@ -26,36 +49,51 @@ object MM {
 
 		val filechannel = FileChannel.open(file, StandardOpenOption.READ)
 		val buffer = filechannel.map(FileChannel.MapMode.READ_ONLY, 0, filechannel.size)
-
-		var last: Long = 0;
+		buffer.order(ByteOrder.LITTLE_ENDIAN)
+		var last: Long = 0
 		while (buffer.isLoaded) {
-			Thread.onSpinWait()
-
-
-			buffer.order(ByteOrder.LITTLE_ENDIAN)
-
-			//			buffer.clear()
-
-
-			var all = 0.0;
+			Thread.sleep(5)
 			val start = buffer.getLong
-
-			//				println(s"Started=$start ~ ${Instant.ofEpochMilli(start)}")
-
 			if (start > last) {
 				last = start
 
+				val ys = Array.ofDim[Particle[Long]](pcount)
+
+
 				time("Read one") {
-					println(s"Size=${buffer.capacity()}")
-					for (i <- 0 until 10000) {
-						val index = buffer.getLong
+					//					println(s"Size=${buffer.capacity()}")
+					for (i <- 0 until pcount) {
+						val t = buffer.getLong
+						val tpe = buffer.getInt
 						val mass = buffer.getFloat
 						val pos = Vec3(buffer.getFloat, buffer.getFloat, buffer.getFloat)
 						val vel = Vec3(buffer.getFloat, buffer.getFloat, buffer.getFloat)
-						//			all += (s"[$i] $index $mass $pos $vel".length)
-						all += (pos distance vel)
+
+						ys(t.toInt) = Particle(t, mass, pos, vel)
+
+
+
+
+
+						//						println(s"[$i] $t $tpe $mass $pos $vel")
 					}
-					println(all)
+
+					def clamp(min: Double, max: Double, value: Double) = Math.max(min, Math.min(max, value))
+
+					Platform.runLater {
+						ys.foreach { y =>
+
+							val p = xs(y.a.toInt)
+
+							p.translateX = y.position.x
+							p.translateY = y.position.y
+							p.translateZ = y.position.z
+							val v = clamp(120, 255, y.velocity.lengthSquared * 100).toInt
+							p.material = new PhongMaterial(Color.rgb(v / 2, v / 2, v, 0.8))
+						}
+
+
+					}
 				}
 
 			} else {
@@ -65,5 +103,35 @@ object MM {
 			buffer.clear()
 		}
 
+		()
+	}).start()
+
+	val subScene = SceneControl.mkScene(new Group(
+		SceneControl.mkAxis(),
+
+	) {
+		children ++= xs.map(_.delegate)
+	}, 500, 500)
+
+
+	stage = new PrimaryStage {
+		title = "SPH simulation"
+		scene = new Scene(new VBox(
+
+			new StackPane {
+				children = Seq(
+					subScene,
+				)
+				subScene.width <== width
+				subScene.height <== height
+				vgrow = Priority.Always
+			},
+
+
+		), 800, 800)
+
+
 	}
+
+
 }
